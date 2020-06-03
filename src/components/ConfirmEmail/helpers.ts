db32cf302694ad29a1f56637e866de526769bdcc
seb-pph <sevatos.matzouranis@peopleperhour.com>
2020-06-03 10:21:55 +0300

    change: login end pont

--- src/components/ConfirmEmail/helpers.ts
+++ src/components/ConfirmEmail/helpers.ts
@@ -2,8 +2,14 @@ import 'babel-polyfill'
 import {makeRequest} from '../../api/fetch'
 
 export const  confirmEmail= async (userId,code)=>{
-  return await makeRequest("GET",`http://api.rem4you.com/api/accounts/ConfirmEmail?userId=${userId}&code=${code}`).then(response=>{
-      console.log("confirmEmail",response)
+  console.log('tests',code)
+  const id = encodeURI(userId)
+  const usercode = encodeURI(code)
+
+  
+  return await makeRequest("GET",`http://api.rem4you.com/api/accounts/ConfirmEmail?userId=${id}&code=${usercode}`).then(response=>{
+    debugger   
+  console.log("confirmEmail",response)
   alert('Email confirmed !')
 
       return response
